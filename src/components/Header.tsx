caf261d8b923d0f602983d4f595203aace934044
thalissongoncalves <thalissongdev@gmail.com>
2025-05-13 17:28:07 -0300

    feat: Adiciona componente Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css"
+
+const Header = () => {
+  return(
+    <div>
+      <h1>Controle Financeiro Pessoal</h1>
+      <h2>Total atual: R$ 2.200</h2>
+    </div>
+  )
+}
+
+export default Header;
