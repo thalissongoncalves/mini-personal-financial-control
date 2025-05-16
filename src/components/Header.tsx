9248033f723b1dd9de6665c4e38488be2d90f7a9
thalissongoncalves <thalissongdev@gmail.com>
2025-05-16 01:28:51 -0300

    style: Adiciona condicional no Header para mudar para verde se for positivo o total value ou vermelho caso ao contrário

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,30 @@
-import "./Header.css"
+import "./Header.css";
 
 interface IHeader {
-  total: number
+  total: number;
 }
 
-const Header = ({total}: IHeader) => {
-  const totalFormated = total.toLocaleString('pt-BR', {
-    style: 'currency',
-    currency: 'BRL'
+const Header = ({ total }: IHeader) => {
+  const totalFormated = total.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
   });
 
-  return(
+  return (
     <header className="headerContainer">
       <h1 className="headerTitle">Controle Financeiro Pessoal</h1>
-      <h2 className="headerSubTitle">Total Atual: <span className="totalValue">{totalFormated}</span></h2>
+      <h2 className="headerSubTitle">
+        Total Atual:{" "}
+        <span
+          className={
+            Number(total) >= 0 ? "totalValueGreen" : "totalValueRed"
+          }
+        >
+          {totalFormated}
+        </span>
+      </h2>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
