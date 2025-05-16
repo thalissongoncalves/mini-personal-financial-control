7c6f17b2b939b00603a4aaf3160f52b363674106
thalissongoncalves <thalissongdev@gmail.com>
2025-05-16 01:12:57 -0300

    feat: Adiciona funcionalidade ao formulário e ao valor total atual

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import { useState } from 'react'
-import './App.css'
-import Filter from './components/Filter'
-import Header from './components/Header'
-import TransactionForm from './components/TransactionForm'
-import TransactionList from './components/TransactionList'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Filter from "./components/Filter";
+import Header from "./components/Header";
+import TransactionForm from "./components/TransactionForm";
+import TransactionList from "./components/TransactionList";
 
-function App() {
-  const [total, setTotal] = useState(0)
-
-  return (
-    <div className='mainContainer'>
-      <Header total={total} />
-      <hr style={{border: '0.1px solid rgba(226, 226, 226, 0.65)', width: '100%'}} />
-      <TransactionForm />
-      <hr style={{border: '0.1px solid rgba(226, 226, 226, 0.65)', width: '100%'}} />
-      <Filter />
-      <hr style={{border: '0.1px solid rgba(226, 226, 226, 0.65)', width: '100%'}} />
-      <TransactionList />
-    </div>
-  )
+export interface IList {
+  description: string,
+  value: string,
+  type: string
 }
 
-export default App
+function App() {
+  const [total, setTotal] = useState(0);
+  const [allList, setAllList] = useState<IList[]>([]);
+  const [revenueList, setRevenueList] = useState<IList[]>([]);
+  const [expenseList, setExpenseList] = useState<IList[]>([]);
+
+  useEffect(() => {
+    revenueList.map((item) => setTotal(total + Number(item.value)))
+    expenseList.map((item) => setTotal(total - Number(item.value)))
+  }, [revenueList, expenseList])
+
+  return (
+    <div className="mainContainer">
+      <Header total={total} />
+      <hr
+        style={{
+          border: "0.1px solid rgba(226, 226, 226, 0.65)",
+          width: "100%",
+        }}
+      />
+      <TransactionForm
+        allList={allList}
+        revenueList={revenueList}
+        expenseList={expenseList}
+        setAllList={setAllList}
+        setRevenueList={setRevenueList}
+        setExpenseList={setExpenseList}
+      />
+      <hr
+        style={{
+          border: "0.1px solid rgba(226, 226, 226, 0.65)",
+          width: "100%",
+        }}
+      />
+      <Filter />
+      <hr
+        style={{
+          border: "0.1px solid rgba(226, 226, 226, 0.65)",
+          width: "100%",
+        }}
+      />
+      <TransactionList />
+    </div>
+  );
+}
+
+export default App;
--- src/components/TransactionForm.tsx
+++ src/components/TransactionForm.tsx
@@ -1,29 +1,84 @@
-import "./TransactionForm.css"
+import { useState } from "react";
+import "./TransactionForm.css";
+import type { IList } from "../App";
 
-const TransactionForm = () => {
-  return(
+interface TransactionFormProps {
+  allList: IList[];
+  revenueList: IList[];
+  expenseList: IList[];
+  setAllList: React.Dispatch<React.SetStateAction<IList[]>>;
+  setRevenueList: React.Dispatch<React.SetStateAction<IList[]>>;
+  setExpenseList: React.Dispatch<React.SetStateAction<IList[]>>;
+}
+
+const TransactionForm = ({
+  allList,
+  revenueList,
+  expenseList,
+  setAllList,
+  setRevenueList,
+  setExpenseList,
+}: TransactionFormProps) => {
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+  const [type, setType] = useState("");
+
+  const handleAddItem = (e: { preventDefault: () => void; }) => {
+    e.preventDefault()
+
+    if (type === "revenue") {
+      setAllList([...allList, {description: description, value: value, type: type}])
+      setRevenueList([...revenueList, {description: description, value: value, type: type}])
+    } else if (type === "expense") {
+      setAllList([...allList, {description: description, value: value, type: type}])
+      setExpenseList([...expenseList, {description: description, value: value, type: type}])
+    }
+    
+    setDescription("")
+    setValue("")
+    setType("")
+  };
+
+  return (
     <form className="form-container">
       <label className="form-group">
         <span>Descrição</span>
-        <input type="text" />
+        <input
+          type="text"
+          name="description"
+          placeholder="Digite aqui. Ex: Conta de água"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
       </label>
       <label className="form-group">
         <span>Valor</span>
-        <input type="number" />
+        <input
+          type="string"
+          name="value"
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
       </label>
       <label className="form-group">
         <span>Tipo</span>
-        <select>
-          <option>Selecione um tipo</option>
-          <option>Receita</option>
-          <option>Despesa</option>
+        <select
+          name="type"
+          onChange={(e) => setType(e.target.value)}
+          value={type}
+        >
+          <option value="" disabled>
+            Selecione um tipo
+          </option>
+          <option value="revenue">Receita</option>
+          <option value="expense">Despesa</option>
         </select>
       </label>
       <div className="form-group button-group">
-        <button>Adicionar</button>
+        <button onClick={handleAddItem}>Adicionar</button>
       </div>
     </form>
-  )
-}
+  );
+};
 
 export default TransactionForm;
